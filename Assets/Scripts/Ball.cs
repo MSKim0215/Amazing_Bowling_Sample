355b7db82dda1344d8b7e7cbbf7116107c753fda
MSKim0215 <rlaalstjqu05@gmail.com>
2022-06-06 17:59:23 +0900

    feat : add to prop physic
    1) 프롭 물리 판정 및 데미지 구현

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [Header("Information")]
+    [SerializeField] private LayerMask targetLayer = default;
     [SerializeField] private float maxDamage = 0f;
     [SerializeField] private float explosionForce = 0f;
     [SerializeField] private float lifeTime = 0f;
@@ -22,6 +23,7 @@ public class Ball : MonoBehaviour
     private void ClearBall()
     {
         // TODO : ¿ÜºÎ¿¡¼­ µ¥ÀÌÅÍ °ª ºÒ·¯¿À´Â °÷
+        targetLayer = LayerMask.GetMask("Prop");
         maxDamage = 100f;
         explosionForce = 1000f;
         lifeTime = 10f;
@@ -33,9 +35,35 @@ public class Ball : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        CheckArea();
         PlayEffect();
         DestroyBall();
     }
+
+    private void CheckArea()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, targetLayer);
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigid = colliders[i].GetComponent<Rigidbody>();
+            targetRigid.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop target = colliders[i].GetComponent<Prop>();
+            float damage = CalculateDamage(colliders[i].transform.position);
+            target.TakeDamage(damage);
+        }
+    }   // ¿µ¿ª Ã¼Å© ÇÔ¼ö , ±è¹Î¼·_220606
+
+    private float CalculateDamage(Vector3 _targetPos)
+    {
+        // TODO : Æø¹ß ÁöÁ¡ °Å¸®¿¡ ºñ·ÊÇÏ¿© µ¥¹ÌÁö
+        Vector3 explosionToTarget = _targetPos - transform.position;
+        float distance = explosionToTarget.magnitude;
+        float edgeToCenterDistance = explosionRadius - distance;
+        float percentage = edgeToCenterDistance / explosionRadius;
+        float damage = maxDamage * percentage;
+        return Mathf.Max(0, damage);
+    }   // µ¥¹ÌÁö °è»ê ÇÔ¼ö , ±è¹Î¼·_220606
 
     private void DestroyBall(float _time = 0f) => Destroy(gameObject, _time);   
     // °ø ÆÄ±« ÇÔ¼ö , ±è¹Î¼·_220606
