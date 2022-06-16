d865f5a271bd485e5c976ce75c09c7593b97d626
MSKim0215 <rlaalstjqu05@gmail.com>
2022-06-16 20:06:17 +0900

    feat : add to ui for power slider

--- Assets/Scripts/Shooter/Shooter.cs
+++ Assets/Scripts/Shooter/Shooter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallShooter : MonoBehaviour
+public class Shooter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
